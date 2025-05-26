2cb58afd7778f3a55c2091aa837a4b2d8d99e028
bronchitis4 <79788949+bronchitis4@users.noreply.github.com>
2025-05-26 12:31:18 +0300

    Update NavigationMenu.js

--- src/components/navigation-menu/NavigationMenu.js
+++ src/components/navigation-menu/NavigationMenu.js
@@ -1,25 +1,44 @@
-
 import './navigationMenu.css';
 import { NavLink, useLocation } from 'react-router-dom';
 import SignOutBtn from '../sign-out/sign-out';
+import { useEffect, useState } from 'react';
+import { getAuth, onAuthStateChanged } from "firebase/auth";
+
 const NavigationMenu = () => {
     const location = useLocation();
+    const [user, setUser] = useState(null);
+    const auth = getAuth();
 
-    if (location.pathname === "/register" || location.pathname === "/login") {
-        return null;
-    }
+    useEffect(() => {
+        const unsubscribe = onAuthStateChanged(auth, (currentUser) => {
+            setUser(currentUser);
+        });
+        return () => unsubscribe(); 
+    }, [auth]);
 
     return (
         <nav className="navigation__menu">
             <span className="logo"><NavLink to="/">LOGO</NavLink></span>
-            <ul className="navigation__list">
-                <li className="list__item"><NavLink to="/chronology">Хронологія</NavLink></li>
-                <li className="list__item"><NavLink to="/events">Події</NavLink></li>
-                <li className="list__item"><NavLink to="/test">Тестування</NavLink></li>
-            </ul>
-            {location.pathname === "/test"  ? <SignOutBtn/> : <div></div>}
+
+            {(location.pathname !== "/register" && location.pathname !== "/login") && (
+                <ul className="navigation__list">
+                    <li className="list__item"><NavLink to="/chronology">Хронологія</NavLink></li>
+                    <li className="list__item"><NavLink to="/events">Події</NavLink></li>
+                    <li className="list__item"><NavLink to="/test">Тестування</NavLink></li>
+                </ul>
+            )}
+
+            {location.pathname !== "/register" && location.pathname !== "/login" ? 
+            (<div className="navigation__right">
+                {user  ? (
+                    location.pathname === "/test" &&  <SignOutBtn />
+                ) : (
+                    <NavLink className="login__btn" to="/login">Увійти</NavLink>
+                )}
+            </div>
+            ) : null}
         </nav>
     );
-}
+};
 
 export default NavigationMenu;
